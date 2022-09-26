d495537e99fc7c800629ffa42fe04da6ba6a7a42
OldBeliver <380506338004@yandex.com>
2022-09-26 17:36:18 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reposition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string title = "Яга";
+            string name = "баба";
+
+            Console.WriteLine($"{title} {name}");
+
+            string tempValue = title;
+            title = name;
+            name = tempValue;
+
+            Console.WriteLine($"{title} {name}");
+        }
+    }
+}
